80a9804c91c86f6ae8259c212d1769cbdbf28a14
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-04 08:09:05 -0500

    Add 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print the string using printf function
+*
+* Description: Using the main function prints
+* "with proper grammar, but the outcome is a piece of art,"
+* Return: 0
+*
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

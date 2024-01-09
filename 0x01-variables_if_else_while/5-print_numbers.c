529d372c0bccfd99b14d393236816e62d1e61491
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-09 05:31:47 -0500

    add 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - display all single digits
+*
+* Description: using the main function, this will
+* display all single digits in base 10.
+* Return: 0
+*
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}

e8a8412c9eedb83c14fd2e5b37f8b0405fb36368
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-10 18:26:22 -0500

    Add 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - a program that prints _putchar
+*
+* Description: using the main function, the program
+* will print _putchar to the console.
+* Return: 0
+*
+*/
+int main(void)
+{
+	char string[] = "_putchar";
+	char *ptr = string;
+
+	while (*ptr != '\0')
+	{
+		printf("%c", *ptr);
+		*ptr++;
+	}
+	return (0);
+}

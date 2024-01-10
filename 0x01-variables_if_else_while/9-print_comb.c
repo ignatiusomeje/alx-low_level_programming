bec29baf29d2534290dc29d512a51af0a063fd57
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-10 08:51:44 -0500

    add 9-print_comb file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the combination of numbers
+ * Description: using main function
+ * this program prints out all possible
+ * combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(48 + i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

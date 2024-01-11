61aff7e7bf03847c72d5e2261b92dcb9f9be2386
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-11 09:16:02 -0500

    add 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 			{
 				if (innercount == 9)
 				{
-					printf("%d", n);
+					printf("%d\n", n);
 				}
 				else
 				{
@@ -30,7 +30,7 @@ void times_table(void)
 			{
 				if (innercount == 9)
 				{
-					printf("%d", n);
+					printf("%d\n", n);
 				}
 				else
 				{
@@ -38,6 +38,6 @@ void times_table(void)
 				}
 			}
 		}
-		printf("\n");
+		//printf("\n");
 	}
 }

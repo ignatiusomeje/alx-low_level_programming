a831f34c62060d6562b9610a83e89e8006823816
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-11 10:31:08 -0500

    add 10-add.c file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* print_to_98 - print natural number to 98;
+* @n: the number to start from;
+* Return: void;
+*/
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, \n", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, \n", n);
+			n--;
+		}
+	}
+	else
+	{
+		printf("%d, \n", n);
+	}
+}

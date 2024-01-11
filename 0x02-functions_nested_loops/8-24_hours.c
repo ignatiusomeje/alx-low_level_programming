dec8652377391538678d35496cd45e51adf63d87
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-11 08:37:38 -0500

    add 8-24_hours.c file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* jack_bauer - print out time
+* Return: void
+*/
+void jack_bauer(void)
+{
+	int hour = 0, mins = 0, msrem, hrem;
+
+	while (hour <= 23)
+	{
+		while (mins <= 53)
+		{
+			msrem = mins % 10;
+			hrem = hour % 10;
+			_putchar((hour / 10) + '0');
+			_putchar(hrem + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar(msrem + '0');
+			mins++;
+			_putchar('\n');
+		}
+		hour++;
+		mins = 0;
+	}
+}

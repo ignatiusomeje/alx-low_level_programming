a08104db5b2663cb4404dd286ac41794e28aebca
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-12 04:04:00 -0500

    add 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_times_table - prints n number of tables
+* starting from 0;
+* @n: the number of tables to prints;
+* Return: void;
+*
+*/
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (!(n < 0 && n > 15))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int mult = i * j;
+
+				if (j == n)
+				{
+					if (mult < 10)
+					{
+						printf("   %d\n", mult);
+					}
+					else
+					{
+						printf("  %d\n", mult);
+					}
+				}
+				else
+				{
+					if (mult < 10)
+					{
+						printf("   %d,", mult);
+					}
+					else
+					{
+						printf("  %d,", mult);
+					}
+				}
+			}
+		}
+	}
+}

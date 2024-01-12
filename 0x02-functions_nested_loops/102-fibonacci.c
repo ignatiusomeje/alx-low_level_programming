a9b4277a7b58281302e38ed839bae166e5e3a41d
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-12 05:18:58 -0500

    add 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - display 50 fibonacci value;
+*
+* Description: using main to display 50
+* fibonacci values;
+* Return: 0;
+*
+*/
+int main(void)
+{
+	unsigned long res, num1 = 0, num2 = 1, i = 0;
+
+	while (i < 50)
+	{
+		res = num1 + num2;
+		if (i == 49)
+		{
+			printf("%ld\n", res);
+		}
+		else
+		{
+			printf("%ld,", res);
+		}
+		num1 = num2;
+		num2 = res;
+		i++;
+	}
+}

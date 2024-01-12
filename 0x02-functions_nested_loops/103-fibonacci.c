b3c58b6009beb4886304a7280e07c03a3826c27c
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-12 06:00:46 -0500

    add 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,18 +9,17 @@
 */
 int main(void)
 {
-	unsigned long res, num1 = 0, num2 = 1, i = 0, sum = 0;
+	unsigned long res, num1 = 0, num2 = 1, sum = 0;
 
 	while (i < 4000000)
 	{
 		res = num1 + num2;
-		if (!(res % 2))
-		{
-			sum += res;
-		}
 		num1 = num2;
 		num2 = res;
-		i++;
+		if ((num1 % 2) == 0)
+		{
+			sum += num1;
+		}
 	}
 	printf("%ld\n", sum);
 	return (0);

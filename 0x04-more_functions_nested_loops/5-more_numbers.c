a222c4ed7cdcc6f8019320af0599f064e1c38dc7
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-13 17:21:09 -0500

    add 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,19 @@
 
 void more_numbers(void)
 {
-  int i = 10;
-  while (i <= 10)
-  {
-    int j = 0;
-    while (j <= 14)
-    { 
-      if (j >= 10)
-        _putchar((j / 10) + '0');
+	int i = 10;
+	while (i <= 10)
+	{
+		int j = 0;
+		while (j <= 14)
+		{ 
+			if (j >= 10)
+				_putchar((j / 10) + '0');
 
-      _putchar((j % 10) + '0');
-      j++;
-    }
-    _putchar('\n');
-    i++;
-  }
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }

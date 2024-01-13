594d5d64ea22d2eb5361116b6a9043d2d0468055
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-13 04:40:25 -0500

    add 0-isdigit.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mul - multiply two numbers together
+* @a: first digit to multiply
+* @b: second digit to multiply
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+	int res = a * b;
+
+	return (res);
+}

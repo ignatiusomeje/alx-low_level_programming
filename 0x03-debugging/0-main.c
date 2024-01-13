cef6f4beda697c26a5be283dfcbce5e1699d388a
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-12 23:35:07 -0500

    )-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* main - test a function;
+* Return: 0
+*/
+int main(void)
+{
+	int j = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef Main_H
+#define Main_H
+
+#include <stdio.h>
+
+void positive_or_nagtive(int i);
+
+#endif /* Main_H*/

d896fa74f9af4f67d4143a1bad4af44fc5072ca8
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-15 07:00:27 -0500

    add 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,3 +15,19 @@ void puts_half(char *str)
 		putchar(*(str + i));
 	putchar(10);
 }
+
+/**
+* _strlen - give the length of a string;
+* @s: the string to give it length;
+* Return: int;
+*/
+
+int _strlen(char *s)
+{
+	int strnum = 0;
+
+	while (*(s + strnum) != '\0')
+		strnum++;
+
+	return (strnum);
+}

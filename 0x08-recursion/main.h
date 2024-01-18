5f08a62e97c4b8c85fcf13ffcf76981b01505988
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-18 10:24:58 -0500

    add 101-wildcmp.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,6 +16,7 @@ int is_prime(int n, int start);
 int is_palindrome(char *s);
 void check(char *s, int start, int end, int *flag);
 int _strlen_recursion(char *s);
+int wildcmp(char *s1, char *s2);
 
 
 #endif

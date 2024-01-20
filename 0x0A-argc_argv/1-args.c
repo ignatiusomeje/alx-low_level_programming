4a32a0772c68d8f871dca686378eb8013d8102e2
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-20 12:26:11 -0500

    add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - using main to print the name of a program;
+* @argc: the number of arguments;
+* @argv: the array of arguments;
+* Return: int;
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

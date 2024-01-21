998cdad0ed5d34b2628c5f56dc9d18655625eaf6
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-21 12:19:23 -0500

    add 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 
-	if (str == 0 || str == NULL)
+	if (size == 0 || str == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)

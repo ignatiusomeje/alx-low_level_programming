4a79199edd8fd759b033edca5172ed02c1996713
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-23 06:35:39 -0500

    Add 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* main - print the current file link
+* Return: int
+*
+*
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,6 @@
+#ifndef __FILE__
+#define __FILE__
+#endif//__FILE__
+#ifndef STDIO
+#include <stdio.h>
+#endif

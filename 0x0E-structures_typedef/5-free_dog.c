8baf732edb730e7743757192827ee854efa2a4c6
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-24 08:18:09 -0500

    Add 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+* free_dog - free the malloc space for a dog;
+* @d: the pointer to the dog's details;
+* Return: void;
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d && d != NULL)
+	{
+		free(d);
+	}
+}

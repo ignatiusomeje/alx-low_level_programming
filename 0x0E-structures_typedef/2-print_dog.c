dfdbc34d8a1995aaa1d62070a1904c22970e4b9b
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-24 06:36:56 -0500

    Add 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+
+/**
+ * print_dog - print the details of a dog
+ * @d: the pointer to the dog's details;
+ * Return: void;
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (d->age == 0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %.1f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}

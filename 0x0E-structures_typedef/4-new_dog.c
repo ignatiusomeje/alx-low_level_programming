3ddf9b9345051c6d3557b8b0616cfead4d30697d
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-24 07:25:13 -0500

    Add 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	if ((name == NULL) || (owner == NULL))
-		return NULL;
+		return (NULL);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)

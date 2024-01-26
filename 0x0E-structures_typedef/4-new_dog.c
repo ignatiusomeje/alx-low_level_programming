b23ca91e7b0a1406d39bb0aa8cd30be5d1f8c17b
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-26 06:28:50 -0500

    Add 4-new_dog file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,44 +1,39 @@
 #include "dog.h"
+#include <string.h>
 
 /**
- * *new_dog - A funtion that creates a new dog
- * @name: the name of the dog;
- * @age: the age of the dog;
- * @owner: the owner of the dog
- * Return: dog_t;
+ * new_dog - creates a new dog
+ * @name: name member of the dog
+ * @age: age member of the dog
+ * @owner: owner member of the dog
+ *
+ * Return: pointer to the new dog_t
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	char *internalname;
-	char *internalowner;
+	char *temp_name;
+	char *temp_owner;
+	dog_t *new_doggo;
 
-	internalname = malloc(sizeof(*name) + 1);
-	internalowner = malloc(sizeof(*owner) + 1);
+	temp_name = malloc(sizeof(*name) * (strlen(name) + 1));
+	temp_owner = malloc(sizeof(*owner) * (strlen(owner) + 1));
+	new_doggo = malloc(sizeof(dog_t));
 
-	if ((internalname == NULL) || (internalowner == NULL))
+	if (temp_name && temp_owner && new_doggo)
 	{
-		free(internalname);
-		free(internalowner);
-		return (NULL);
+		strcpy(temp_name, name);
+		strcpy(temp_owner, owner);
+		new_doggo->name = temp_name;
+		new_doggo->age = age;
+		new_doggo->owner = temp_owner;
 	}
 	else
 	{
-		internalname = name;
-		internalowner = owner;
-		dog = malloc(sizeof(dog_t));
-		if (dog == NULL)
-		{
-			free(dog);
-			return (NULL);
-		}
-		else
-		{
-			dog->name = internalname;
-			dog->age = age;
-			dog->owner = internalowner;
-			return (dog);
-		}
+		free(temp_name);
+		free(temp_owner);
+		free(new_doggo);
+		return (NULL);
 	}
+	return (new_doggo);
 }
